4af8683b22b8830977a1523d71d9b72318fd0692
gasinvein <gasinvein@gmail.com>
2018-12-20 16:22:59 +0300

    Update apply_extra

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-FO_DIR=$1
-FO_VER="18"
-APPID="$2"
+set -x
+
+APP_ID="com.softmaker.FreeOffice"
+
+tar -xf freeoffice.tgz
+mkdir freeoffice
+tar -C freeoffice -xf freeoffice2018.tar.lzma
 
 declare -A abbreviations
 abbreviations=(
-    [textmaker]="tml"
-    [planmaker]="pml"
-    [presentations]="prl"
+    [textmaker]="tm"
+    [planmaker]="pm"
+    [presentations]="pr"
 )
 
+fullname_base="FreeOffice 2018"
 declare -A fullnames
 fullnames=(
-    [textmaker]="FreeOffice 2018 TextMaker"
-    [planmaker]="FreeOffice 2018 PlanMaker"
-    [presentations]="FreeOffice 2018 Presentations"
+    [textmaker]="$fullname_base TextMaker"
+    [planmaker]="$fullname_base PlanMaker"
+    [presentations]="$fullname_base Presentations"
 )
 
 app_icon_sizes=(16 24 32 48 64 72 128 256 512 1024)
@@ -22,31 +27,30 @@ mime_icon_sizes=(16 24 32 48 64 96 128 256 512 1024)
 
 for app in textmaker planmaker presentations; do
     a=${abbreviations[$app]}
-    # install .desktop file
-    desktop_file="/app/share/applications/$APPID-$app.desktop"
-    install -Dm644 "$FO_DIR/mime/${a}${FO_VER}.dsk" "$desktop_file"
-    {
-        echo "Version=1.0";
-        echo "Name=${fullnames[$app]}";
-        echo "Icon=$APPID-$app";
-        echo "Exec=$app %F";
-        echo "TryExec=$app";
-        echo "StartupWMClass=${a/l/}";
-    } >> "$desktop_file"
+    # Install desktop entry
+    desktop_file="export/share/applications/$APP_ID-$app.desktop"
+    install -Dm644 "freeoffice/mime/${a}l18.dsk" "$desktop_file"
+    desktop-file-edit --set-key="Name" --set-value="${fullnames[$app]}" "$desktop_file"
+    desktop-file-edit --set-key="Icon" --set-value="$APP_ID-$app" "$desktop_file"
+    desktop-file-edit --set-key="Exec" --set-value="$app %F" "$desktop_file"
+    desktop-file-edit --set-key="TryExec" --set-value="$app" "$desktop_file"
+    desktop-file-edit --set-key="StartupWMClass" --set-value="${a}" "$desktop_file"
     # install application icons
     for s in "${app_icon_sizes[@]}"; do
-        install -Dm644 "$FO_DIR/icons/${a}_${s}.png" "/app/share/icons/hicolor/${s}x${s}/apps/$APPID-$app.png"
+        install -Dm644 "freeoffice/icons/${a}l_${s}.png" "export/share/icons/hicolor/${s}x${s}/apps/$APP_ID-$app.png"
     done
 done
 
-install -Dm644 "$FO_DIR/mime/softmaker-freeoffice${FO_VER}.xml" "/app/share/mime/$APPID.xml"
+install -Dm644 "freeoffice/mime/softmaker-freeoffice18.xml" "export/share/mime/packages/$APP_ID.xml"
 
 for t in tmd tmd_mso tmd_oth pmd pmd_mso pmd_oth prd prd_mso prd_oth; do
     suffix=${t/_/-}
     # install mime icons
     for s in "${mime_icon_sizes[@]}"; do
-        install -Dm644 "$FO_DIR/icons/${t}_${s}.png" "/app/share/icons/hicolor/${s}x${s}/mimetypes/$APPID-$suffix.png"
+        install -Dm644 "freeoffice/icons/${t}_${s}.png" "export/share/icons/hicolor/${s}x${s}/mimetypes/$APP_ID-$suffix.png"
     done
     # change icon names in mimeinfo file
-    sed "s/application-x-$suffix/$APPID-$suffix/g" -i "/app/share/mime/$APPID.xml"
+    sed "s/icon name=\"application-x-$suffix\"/icon name=\"$APP_ID-$suffix\"/g" -i "export/share/mime/packages/$APP_ID.xml"
 done
+
+rm freeoffice.tgz freeoffice2018.tar.lzma installfreeoffice
