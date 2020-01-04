30184b7f749d12913815508f135db1e6b77def08
gasinvein <gasinvein@gmail.com>
2020-01-04 23:58:16 +0300

    Remove trash, fix apply_extra

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,26 +1,32 @@
-#!/bin/bash -x
+#!/bin/bash
+set -e
+FLATPAK_ID=com.teamviewer.TeamViewer
 TV_VER_MAJOR=14
 
 mkdir -p teamviewer export/share/{dbus-1/services,polkit-1/actions,applications}
 tar -xf teamviewer.tar.xz -C teamviewer --strip-components=1
 
-install -Dm644 teamviewer/tv_bin/script/com.teamviewer.TeamViewer{,.Desktop}.service -t export/share/dbus-1/services/
-install -Dm644 teamviewer/tv_bin/desktop/com.teamviewer.TeamViewer.desktop -t export/share/applications/
-#install -Dm644 teamviewer/tv_bin/script/com.teamviewer.TeamViewer.policy -t export/share/polkit-1/actions/
-for res in 16 20 24 32 48 256; do
-    install -Dm644 teamviewer/tv_bin/desktop/teamviewer_$res.png export/share/icons/hicolor/${res}x${res}/apps/com.teamviewer.TeamViewer.png
+install -Dm644 "teamviewer/tv_bin/script/${FLATPAK_ID}"{,.Desktop}.service -t export/share/dbus-1/services/
+install -Dm644 "teamviewer/tv_bin/desktop/${FLATPAK_ID}.desktop" -t export/share/applications/
+for s in 16 20 24 32 48 256; do
+    install -Dm644 "teamviewer/tv_bin/desktop/teamviewer_$s.png" \
+                   "export/share/icons/hicolor/${s}x${s}/apps/${FLATPAK_ID}.png"
 done
-for f in export/share/{dbus-1/services/com.teamviewer.TeamViewer{,.Desktop}.service,applications/com.teamviewer.TeamViewer.desktop}; do
-    sed 's|/opt/teamviewer/|/app/extra/teamviewer/|g' -i $f
+for f in export/share/{"dbus-1/services/${FLATPAK_ID}"{,.Desktop}.service,"applications/${FLATPAK_ID}.desktop"}; do
+    sed "s|/opt/teamviewer/|/app/extra/teamviewer/|g" -i "$f"
 done
-sed 's|Icon=.*|Icon=com.teamviewer.TeamViewer|g' -i export/share/applications/com.teamviewer.TeamViewer.desktop
+sed "s|Icon=.*|Icon=${FLATPAK_ID}|g" -i "export/share/applications/${FLATPAK_ID}.desktop"
 
-sed 's|TAR_NI|TAR_IN|g' -i teamviewer/tv_bin/script/tvw_config
+sed \
+    -e "s|TAR_NI|TAR_IN|g" \
+    -e "s|/opt/teamviewer/|/app/extra/teamviewer/|g" \
+    -e "s|.config/|.var/app/${FLATPAK_ID}/config/|g" \
+    -e "s|.local/share/|.var/app/${FLATPAK_ID}/data/|g" \
+    -i teamviewer/tv_bin/script/tvw_config
 
 rmdir teamviewer/{config,logfiles}
-#FIXME Using HOME path here is probably bad
-ln -s "$HOME/.config/teamviewer" teamviewer/config
-ln -s "$HOME/.local/share/teamviewer${TV_VER_MAJOR}/logfiles" teamviewer/logfiles
-ln -s "$HOME/.local/share/teamviewer${TV_VER_MAJOR}" teamviewer/profile
+ln -s "/var/config/teamviewer" teamviewer/config
+ln -s "/var/data/teamviewer${TV_VER_MAJOR}/logfiles" teamviewer/logfiles
+ln -s "/var/data/teamviewer${TV_VER_MAJOR}" teamviewer/profile
 
 rm teamviewer.tar.xz
--- teamviewer-wrapper.sh
+++ teamviewer-wrapper.sh
@@ -5,4 +5,4 @@ for d in config logfiles profile; do
     mkdir -p "$(readlink -m "/app/extra/teamviewer/$d")"
 done
 
-exec /app/extra/teamviewer/tv_bin/script/teamviewer $@
+exec /app/extra/teamviewer/tv_bin/script/teamviewer "$@"
